38c056b3bbec4f107a1df9247967fa74be315286
TwoRice <2rimagowan@gmail.com>
2023-12-16 14:04:21 +0000

    day 16 solution

--- day16/day16.py
+++ day16/day16.py
@@ -1,52 +1,75 @@
 import numpy as np
 import sys
+sys.setrecursionlimit(100000)
 
+RIGHT = (0, 1)
+LEFT = (0, -1)
+DOWN = (1, 0)
+UP = (-1, 0)
+movements = {
+    "|": {
+        RIGHT: [UP, DOWN],
+        LEFT: [UP, DOWN],
+        DOWN: [DOWN],
+        UP: [UP]
+    },
+    "-": {
+        RIGHT: [RIGHT],
+        LEFT: [LEFT],
+        DOWN: [LEFT, RIGHT],
+        UP: [LEFT, RIGHT]
+    },
+    "/": {
+        RIGHT: [UP],
+        LEFT: [DOWN],
+        DOWN: [LEFT],
+        UP: [RIGHT]
+    },
+    "\\": {
+        RIGHT: [DOWN],
+        LEFT: [UP],
+        DOWN: [RIGHT],
+        UP: [LEFT]
+    },
+    ".": {
+        RIGHT: [RIGHT],
+        LEFT: [LEFT],
+        DOWN: [DOWN],
+        UP: [UP]
+    }
+}
 
 def move_light(y, x, direction, grid, visited):
-    visited.append((y, x, direction))
+    loop = (y, x, direction) in visited
+    visited.add((y, x, direction))
     y, x = np.array([y, x]) + np.array(direction)
 
-    if y in [-1, len(grid)] or x in [-1, len(grid[0])] or (y, x, direction) in visited:
-        return
+    if y in [-1, len(grid)] or x in [-1, len(grid[0])] or loop:
+        return visited
 
-    if grid[y, x] == "|":
-        if abs(direction[0]) == 0:
-            move_light(y, x, (1, 0), grid, visited)
-            move_light(y, x, (-1, 0), grid, visited)
-        else:
-            move_light(y, x, direction, grid, visited)
-    elif grid[y, x] == "-":
-        if abs(direction[0]) == 1:
-            move_light(y, x, (0, 1), grid, visited)
-            move_light(y, x, (0, -1), grid, visited)
-        else:
-            move_light(y, x, direction, grid, visited)
-    elif grid[y, x] == "/":
-        if direction[0] == 1:
-            move_light(y, x, (0, -1), grid, visited)
-        elif direction[0] == -1:
-            move_light(y, x, (0, 1), grid, visited)
-        elif direction[1] == 1:
-            move_light(y, x, (-1, 0), grid, visited)
-        else:
-            move_light(y, x, (1, 0), grid, visited)
-    elif grid[y, x] == "\\":
-        if direction[0] == 1:
-            move_light(y, x, (0, 1), grid, visited)
-        elif direction[0] == -1:
-            move_light(y, x, (0, -1), grid, visited)
-        elif direction[1] == 1:
-            move_light(y, x, (1, 0), grid, visited)
-        else:
-            move_light(y, x, (-1, 0), grid, visited)
-    else:
-        move_light(y, x, direction, grid, visited)
+    new_direction = movements[grid[y, x]][direction]
+    for dir in new_direction:
+        visited.update(move_light(y, x, dir, grid, visited))
+    return visited
+
+def calculate_energy(start_pos, direction, grid):
+    visited = set()
+    visited = move_light(*start_pos, direction, grid, visited)
+    return len({(y, x) for y, x, d in visited})
 
 if __name__ == "__main__":
     sys.setrecursionlimit(5000)
     with open("day16.txt", "r") as f:
         contraption = np.array([list(row) for row in f.read().split("\n")])
 
-    visited = []
-    move_light(0, -1, (0, 1), contraption, visited)
-    print(f"Part 1: {len({(y, x) for y, x, d in visited}) - 1}")
+    print(f"Part 1: {calculate_energy((0, -1), (0, 1), contraption)}")
+    
+    num_energised = []
+    for y in range(len(contraption)):
+        num_energised.append(calculate_energy((y, 0), (0, 1), contraption))
+        num_energised.append(calculate_energy((y, len(contraption[0])-1), (0, -1), contraption))
+    for x in range(len(contraption[0])):
+        num_energised.append(calculate_energy((0, x), (1, 0), contraption))
+        num_energised.append(calculate_energy((len(contraption)-1, x), (-1, 0), contraption))
+        
+    print(f"Part 2: {max(num_energised)}")
